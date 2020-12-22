b11b7948dffcac7aa31c38bf51b184825cbce1d2
vagrant <rik.s.1005@gmail.com>
2020-12-22 14:01:51 +0000

    [update]top,aboutページ作成、ヘッダーにログアウトリンク

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#top'
+  get '/home/about' => 'home#about'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
